4d788429caefaff98729c21b139478f11ba92630
pdf <pedrodiasfonseca@gmail.com>
2019-04-19 11:28:49 -0300

    feat(WhoWeAreSection): Create component that receives info about program volunteers

---
+++ components/WhoWeAreSection/WhoWeAreSection.jsx
@@ -0,0 +1,202 @@
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import Title from '../Title'
+
+function WhoWeAreSection () {
+  return (
+    <div style={{ paddingTop: 96, paddingBottom: 96 }}>
+      <Container>
+        <Row>
+          <Col>
+            <Title arrows as='h2'>Quem somos</Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+          <Col lg={4} sm={6} className="mt-4">
+            <Row bsPrefix="row align-items-center">
+              <Col xs={4}>
+                <img src="http://i.pravatar.cc/80" className="img-fluid rounded-circle mx-auto d-block" />
+              </Col>
+              <Col xs={8}>
+                <p className="h5 mb-0">[Nome do voluntário]</p>
+                <em>[Profissão do voluntário]</em>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+WhoWeAreSection.propTypes = {}
+
+export default WhoWeAreSection
