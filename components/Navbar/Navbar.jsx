dbe653689c2f28a92cd6ef1b614ddef7726e8e01
pdf <pedrodiasfonseca@gmail.com>
2020-08-01 12:27:14 -0300

    refactor: conveert Navbar to tailwind

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,45 +1,63 @@
-import React from 'react'
-import BsNavbar, { Brand, Toggle, Collapse } from 'react-bootstrap/Navbar'
-import BsNav, { Link as NavLink } from 'react-bootstrap/Nav'
+import React, { useState } from 'react'
 import Link from 'next/link'
-import Container from 'react-bootstrap/Container'
-
-import { navbarStyle } from './Navbar.style'
 
 function Navbar () {
-  const navbarId = 'navbar-menu';
-  const { className, styles } = navbarStyle
+  const [open, setOpen] = useState(false)
+
+  const toggleMenu = () => setOpen(!open)
 
   return (
-    <BsNavbar bg='transparent' expand='lg' className={className}>
-      <Container>
-        <Link href='/'>
-          <Brand>
-            <img src='/static/logo.png' alt='Programadores do futuro' />
-          </Brand>
-        </Link>
-        <Toggle aria-controls={`${navbarId}`} />
-        <Collapse id={`${navbarId}`}>
-          <BsNav className='ml-auto'>
-            <NavLink href='#sobre-o-projeto'>
-              O PROJETO
-            </NavLink>
-            <NavLink href='#como-funciona'>
-              COMO FUNCIONA
-            </NavLink>
-            <NavLink href='#quem-somos'>
-              QUEM SOMOS
-            </NavLink>
-            <Link href='/redirecionar?utm_source=navbar' passHref>
-              <NavLink>
-                COLABORAR
-              </NavLink>
+    <div className='container mx-auto pt-4 px-4 xl:px-0'>
+      <nav className='flex items-center justify-between flex-wrap py-6'>
+        <div className='flex items-center flex-shrink-0 text-white mr-6'>
+          <img src='/static/logo.png' alt='Programadores do amanhã' />
+        </div>
+        <div className='block lg:hidden'>
+          <button
+            onClick={toggleMenu}
+            className='flex items-center px-3 py-2 border-2 rounded text-white border-white hover:text-white hover:border-white'
+          >
+            <svg
+              className='fill-current h-4 w-4'
+              viewBox='0 0 20 20'
+              xmlns='http://www.w3.org/2000/svg'
+            >
+              <title>Menu</title>
+              <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+            </svg>
+          </button>
+        </div>
+        <div
+          className={`${!open ? 'hidden' : ''} w-full block flex-grow lg:flex lg:items-center lg:w-auto text-left lg:text-right`}
+        >
+          <div className='text-sm lg:flex-grow'>
+            <Link href='#sobre-o-projeto'>
+              <a className='block mt-4 lg:inline-block lg:mt-0 text-base text-white hover:text-white mr-4'>
+                O PROJETO
+              </a>
             </Link>
-          </BsNav>
-        </Collapse>
-        { styles }
-      </Container>
-    </BsNavbar>
+            <Link href='#como-funciona'>
+              <a className='block mt-4 lg:inline-block lg:mt-0 text-base text-white hover:text-white mr-4'>
+                COMO FUNCIONA
+              </a>
+            </Link>
+            <Link href='#quem-somos'>
+              <a
+                href='#responsive-header'
+                className='block mt-4 lg:inline-block lg:mt-0 text-base text-white hover:text-white'
+              >
+                QUEM SOMOS
+              </a>
+            </Link>
+            <Link href='/redirecionar?utm_source=navbar'>
+              <a className='block mt-4 lg:inline-block lg:mt-0 text-base text-white hover:text-white'>
+                COLABORAR
+              </a>
+            </Link>
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
